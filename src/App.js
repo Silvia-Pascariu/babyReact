7bb0fa2b5b0bbab9aea5bae279715144d1df449d
spascariu <silvia.pascariu@made.com>
2020-04-02 10:40:27 +0300

    folder structure commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./navbar/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <NavBar></NavBar>
+                <h2>Baby Shine App</h2>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
